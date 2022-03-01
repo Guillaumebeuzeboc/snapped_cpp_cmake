8a09e8ea59b9a320db802c40a66cd0c16e83df2b
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2022-03-01 11:01:17 +0100

    feat(app): confined app can see files and directory

--- main.cpp
+++ main.cpp
@@ -2,20 +2,16 @@
 #include <string>
 
 #include <boost/filesystem.hpp>
+#include <boost/range/iterator_range.hpp>
 
 int main(){
-    std::cout << "My C++ snaped app is launched from --> " << boost::filesystem::current_path() << std::endl;
-    const auto path = boost::filesystem::current_path() / "hello";
-    auto ret = boost::filesystem::create_directory(path);
-    if(!ret) {
-        std::cout << "Failed to create directory" << std::endl;
-        return -1;
-    }
-    std::cout << "Filesystem accessed!" << std::endl;
-    ret = boost::filesystem::remove(path);
-    if(!ret) {
-        std::cout << "Failed to remove the directory" << std::endl;
-        return -1;
-    }
+    const auto path = boost::filesystem::current_path();
+    std::cout << "My C++ snaped app is launched from --> " << path << std::endl;
+    std::cout << "Is snap/ a directory ? " << boost::filesystem::is_directory(path / "snap") << std::endl;
+    if(is_regular_file(path / "main.cpp"))
+	std::cout << "There is a main.cpp file" << std::endl;
+    else
+	std::cout << "No main.cpp file :(" << std::endl;
+
     return 0;
 }
